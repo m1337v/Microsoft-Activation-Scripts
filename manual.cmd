d382ced5dd8e6006ac5fc01169b28f0e795907ab
m1337v <xxmichaelsxx@googlemail.com>
2023-12-26 18:34:21 -0600

    Update manual.cmd

--- manual.cmd
+++ manual.cmd
@@ -1,3 +1,3 @@
-slmgr /ipk <> #insert key e.g. W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /ipk <> #insert key e.g. W269N-WFGWX-YVC9B-4J6C9-T83GX check https://learn.microsoft.com/ru-ru/windows-server/get-started/kms-client-activation-keys
 slmgr /skms kms.digiboy.ir
 slmgr /ato
